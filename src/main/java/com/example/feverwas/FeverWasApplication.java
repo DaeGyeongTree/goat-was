2d1c78e10c26616499a4fd22519edc3af18d22ed
Jinwoo Lee <binaryWooo@gmail.com>
2023-09-11 09:43:50 +0900

    chore: init spring project

---
+++ src/main/java/com/example/feverwas/FeverWasApplication.java
@@ -0,0 +1,13 @@
+package com.example.feverwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeverWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeverWasApplication.class, args);
+	}
+
+}
