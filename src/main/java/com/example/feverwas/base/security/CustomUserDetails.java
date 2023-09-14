008caf4f282cf6632f0f31787928fd144e958a5e
Jinwoo Lee <binaryWooo@gmail.com>
2023-09-14 15:27:34 +0900

    feat: implement jwt-provider

---
+++ src/main/java/com/example/feverwas/base/security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.feverwas.base.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.feverwas.boundedContext.auth.entity.Member;
+
+public class CustomUserDetails implements UserDetails {
+	private final Member member;
+
+	public CustomUserDetails(Member member) {
+		this.member = member;
+	}
+
+	public final Member getMember() {
+		return member;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return member.getEmail();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
---
+++ src/main/java/com/example/feverwas/base/security/JpaUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.feverwas.base.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.example.feverwas.boundedContext.auth.entity.Member;
+import com.example.feverwas.boundedContext.auth.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JpaUserDetailsService implements UserDetailsService {
+	private final MemberRepository memberRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberRepository.findByEmail(username);
+		if (member != null) {
+			return new CustomUserDetails(member);
+		} else {
+			return (UserDetails) new UsernameNotFoundException("User not found with username: " + username);
+		}
+
+	}
+}
