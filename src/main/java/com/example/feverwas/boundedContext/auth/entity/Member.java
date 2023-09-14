6cfed91cc2a933f7989303719dcaaf32b7b1b2e7
Jinwoo Lee <binaryWooo@gmail.com>
2023-09-14 14:15:17 +0900

    feat: implement member-entity

---
+++ src/main/java/com/example/feverwas/boundedContext/auth/entity/Member.java
@@ -0,0 +1,21 @@
+package com.example.feverwas.boundedContext.auth.entity;
+
+import com.example.feverwas.base.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member extends BaseEntity {
+	@Column(length = 100, nullable = false, unique = true)
+	private String email;
+
+	@Column(length = 50, nullable = false)
+	private String nickname;
+}
