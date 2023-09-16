24d71d73e50466b7922a5eae51bbc298d3226d24
Jinwoo Lee <binaryWooo@gmail.com>
2023-09-17 01:09:39 +0900

    fix: add recipe-ingredient

--- src/main/java/com/example/feverwas/boundedContext/recipe/entity/Recipe.java
+++ src/main/java/com/example/feverwas/boundedContext/recipe/entity/Recipe.java
@@ -6,8 +6,10 @@ import com.example.feverwas.base.entity.BaseEntity;
 import com.example.feverwas.boundedContext.auth.entity.Member;
 import com.example.feverwas.boundedContext.ingredient.entity.IngredientCategory;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -32,7 +34,7 @@ public class Recipe extends BaseEntity {
 	@JoinColumn(name = "member_id", nullable = false)
 	private Member member;
 
-	@OneToMany
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "recipe")
 	private List<RecipeIngredient> recipeIngredient;
 
 	@Builder
